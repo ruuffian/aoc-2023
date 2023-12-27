b5193f0f616a3b36c36cf62e83d8dfefbe301274
ruuffian <ruuffia@gmail.com>
2023-12-27 11:50:50 +0000

    Minor improvements to alg with Stream and some .all calls

--- elixir/day2.exs
+++ elixir/day2.exs
@@ -25,13 +25,13 @@ part1 = fn ->
     check = %{"r" => 12, "g" => 13, "b" => 14}
     # Split per game step
     String.split(counts, ",")
-    |> Enum.reduce(:true, fn picked_cubes, valid_cube_count ->
+    |> Enum.all?(fn picked_cubes ->
       String.split(picked_cubes, " ")
       |> (fn cube ->
-        # Grab the cube
+        # Validate individual cubes
         cube_count = hd(cube)
-        cube_colour = tl(cube) |> List.to_string()
-        valid_cube_count and (cube_count |> String.to_integer() <= Map.get(check, cube_colour))
+        cube_colour = cube |> Enum.reverse() |> hd()
+        cube_count |> String.to_integer() <= Map.get(check, cube_colour)
       end).()
     end)
   end
@@ -39,53 +39,52 @@ part1 = fn ->
   # Returns true if a round is valid given the rules and false otherwise
   process_round = fn round ->
     round
-    |> Enum.map(& check_cubes.(&1))
-    |> Enum.member?(:false)
-    |> (& not &1).()
+    |> Stream.map(& check_cubes.(&1))
+    |> Enum.all?()
   end
 
   # Read input, split on line, format it
   Day2.read_in()
-  |> Enum.reduce([0], fn round, count ->
+  |> Stream.with_index()
+  |> Enum.reduce(0, fn {round, index}, count ->
     process_round.(round)
     |> if do
-      [length(count) | count]
+        count + index + 1
       else
-        [0 | count]
+        count
     end
   end)
-  |> Enum.reduce(& &1 + &2)
   |> IO.inspect()
 end
+part2 = fn ->
 
-max_cubes = fn round ->
-  maxes = %{"b" => 0, "r" => 0, "g" => 0}
-  round
-  |> Enum.map(fn step ->
-    step
-    |> String.split(",")
-    |> Enum.reduce(maxes, fn cube, cube_max ->
-        colour = Regex.run(~r/(b|r|g)/, cube) |> hd()
-        tmp = Regex.run(~r/[\d]*/, cube) |> List.to_string() |> String.to_integer()
-        Map.update!(cube_max, colour, fn curr -> max(tmp, curr)
-      end)
-    end)
-  end)
-end
-
-combine_cube_maxes = fn round ->
-  base = %{"b" => 0, "r" => 0, "g" => 0}
-
-  round
-  |> Enum.reduce(base, fn cube, cube_acc ->
-    cube
-    |> Enum.reduce(cube_acc, fn {colour, _}, tmp ->
-        Map.update!(tmp, colour, & max(&1, Map.get(cube, colour)))
+  max_cubes = fn round ->
+    base = %{"b" => 0, "r" => 0, "g" => 0}
+    round
+    |> Stream.map(fn step ->
+      step
+      |> String.split(",")
+      |> Enum.reduce(base, fn cube, cube_max ->
+          colour = Regex.run(~r/(b|r|g)/, cube) |> hd()
+          tmp = Regex.run(~r/[\d]*/, cube) |> List.to_string() |> String.to_integer()
+          Map.update!(cube_max, colour, fn curr -> max(tmp, curr)
+        end)
       end)
     end)
   end
 
-part2 = fn ->
+  combine_cube_maxes = fn round ->
+    base = %{"b" => 0, "r" => 0, "g" => 0}
+
+    round
+    |> Enum.reduce(base, fn cube, cube_acc ->
+      cube
+      |> Enum.reduce(cube_acc, fn {colour, _}, tmp ->
+          Map.update!(tmp, colour, & max(&1, Map.get(cube, colour)))
+        end)
+      end)
+    end
+
   Day2.read_in()
   |> Enum.map(& max_cubes.(&1))
   |> Enum.map(& combine_cube_maxes.(&1))
